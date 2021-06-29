97b60d8a3662acb1aee2f92159f6dd11ae676384
Your name <you@example.com>
2021-06-29 10:49:20 -0400

    commit tree 414ef04f8f6519620ed62f63ab4dece2f2fdf97d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+class School
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+   
+def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+def grades(stu_grade)
+    roster[stu_grade]
+end
+
+def sort
+    sorted_array = {}
+
+    roster.each do |grade, student|
+        sorted_array[grade]= student.sort
+    end
+end
+# binding.pry
+end
+
